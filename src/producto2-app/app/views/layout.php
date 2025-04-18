fdef2e6bbaa6143205d57e64c05c217b1fb8dcec
aantonpan <aantonpan@uoc.edu>
2025-04-18 16:59:46 +0200

    style(Auth): white  label.form-label

--- src/producto2-app/app/views/layout.php
+++ src/producto2-app/app/views/layout.php
@@ -19,8 +19,8 @@ if (!empty($_GET['modal'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
 
-    <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="/css/style.css">
 
 
 </head>
